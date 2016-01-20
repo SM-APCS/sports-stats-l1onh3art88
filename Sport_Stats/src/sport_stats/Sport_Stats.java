371ec5bd4a88e791ca0d9ce8ff553c17ef3d3682
Kurt <Kurt@10.29.1.10>
2016-01-20 11:27:53 -0600

    updating assignment details

---
+++ Sport_Stats/src/sport_stats/Sport_Stats.java
@@ -0,0 +1,18 @@
+package sport_stats;
+
+/**
+ * Create at least 10 different sports. 
+ * You should have more stats than sports 
+ * since multiple sports have common attributes.
+ * @author your name here
+ */
+public class Sport_Stats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
